7721561fb1ee8bc054b8f111a659a550ebe15ade
Bruno Martins da Silva <brunomartins.sc@gmail.com>
2018-01-02 21:45:52 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-ms-in-action
---
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -0,0 +1,26 @@
+package com.thoughtmechanix.licenses.controllers;
+
+import com.thoughtmechanix.licenses.config.ServiceConfig;
+import com.thoughtmechanix.licenses.services.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping(value="v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+
+    @Autowired
+    private LicenseService licenseService;
+
+    @Autowired
+    private ServiceConfig serviceConfig;
+
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public String teste() {
+
+        return serviceConfig.getExampleProperty();
+    }
+
+}
